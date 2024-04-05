211cd67b540e16c3a3db463649f8a4bef57975e7
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-04-05 19:07:09 +0530

    Made the header responsive and added the menu btn

--- client/src/component/Header/Header.jsx
+++ client/src/component/Header/Header.jsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
+import { BiMenuAltRight } from "react-icons/bi";
+import OutsideClickHandler from "react-outside-click-handler";
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuBtnAction = () => {
+    setMenuOpen((prev) => !prev);
+  };
+
+  const getMenuStyles = (menuOpen) => {
+    if (document.documentElement.clientWidth <= 800) {
+      return { right: !menuOpen && "-100%" };
+    }
+  };
   return (
     <section className="h-wrapper">
       <div className="flexCenter paddings innerWidth h-container">
         <img src="./logo.png" alt="logo" width={100} />
 
-        <div className="flexCenter h-menu">
-          <a href="">Residencies</a>
-          <a href="">Our Value</a>
-          <a href="">Conatct Us</a>
-          <a href="">Get Started</a>
-          <button className="button">
-            <a href="">Contacts</a>
-          </button>
+        <OutsideClickHandler  onOutsideClick={() => setMenuOpen(false)}>
+          <div className="flexCenter h-menu" style={getMenuStyles(menuOpen)}>
+            <a href="">Residencies</a>
+            <a href="">Our Value</a>
+            <a href="">Conatct Us</a>
+            <a href="">Get Started</a>
+            <button className="button">
+              <a href="">Contacts</a>
+            </button>
+          </div>
+        </OutsideClickHandler>
+
+        <div className="menu-icon" onClick={handleMenuBtnAction}>
+          <BiMenuAltRight size={30} />
         </div>
       </div>
     </section>
