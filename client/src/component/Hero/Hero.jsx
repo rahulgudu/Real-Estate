01fe32ab49bdc327e7c3dfdb3355b772bb136f8c
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-04-06 20:05:12 +0530

    Added animation to the hero section

--- client/src/component/Hero/Hero.jsx
+++ client/src/component/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Hero.css";
 import { HiLocationMarker } from "react-icons/hi";
 import CountUp from "react-countup";
+import { motion } from "framer-motion";
 const Hero = () => {
   return (
     <section className="hero-wrapper">
@@ -10,15 +11,26 @@ const Hero = () => {
         <div className="flexColStart hero-left">
           <div className="hero-title">
             <div className="orange-circle"></div>
-            <h1>
+            <motion.h1
+              initial={{ y: "2rem", opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{
+                duration: 2,
+                type: "spring",
+              }}
+            >
               Discover <br />
               Most Suitable <br /> Property
-            </h1>
+            </motion.h1>
           </div>
 
           <div className="flexColStart hero-des">
-            <span className="secondaryText">Find a variety of properties that suit you very easily</span>
-            <span className="secondaryText">Forget all difficulties in finding a resistance for you</span>
+            <span className="secondaryText">
+              Find a variety of properties that suit you very easily
+            </span>
+            <span className="secondaryText">
+              Forget all difficulties in finding a resistance for you
+            </span>
           </div>
 
           <div className="flexCenter search-bar">
@@ -44,7 +56,7 @@ const Hero = () => {
             </div>
             <div className="flexColCenter stat">
               <span>
-                <CountUp  end={28} />
+                <CountUp end={28} />
                 <span>+</span>
               </span>
               <span className="secondaryText">Awards</span>
@@ -54,9 +66,17 @@ const Hero = () => {
 
         {/* Right Section */}
         <div className="flexCenter hero-right">
-          <div className="image-container">
+          <motion.div
+            initial={{ x: "7rem", opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{
+              duration: 2,
+              speed: "spring",
+            }}
+            className="image-container"
+          >
             <img src="./hero-image.png" alt="" />
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
