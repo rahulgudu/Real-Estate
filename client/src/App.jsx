a74d74d3f1714f13972ce6a1edd5e8b9e13ba102
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-04-17 21:06:01 +0530

    created the template of properties page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,15 @@ import Website from "./pages/Website";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Layout from "./component/Layout/Layout";
+import Properties from "./pages/Properties/Properties";
 function App() {
   return (
     <BrowserRouter>
       <Suspense fallback={<div>loading....</div>}>
         <Routes>
           <Route element={<Layout />}>
-            <Route path="/" element={<Website />}></Route>
+            <Route path="/" element={<Website />} />
+            <Route path="/properties" element={<Properties />} />
           </Route>
         </Routes>
       </Suspense>
