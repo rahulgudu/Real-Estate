31ba0994f7ef466b08b5d3e9cc6ff02811ed9f5b
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-04-20 09:19:47 +0530

    properties page updated and fixed the bug at srvr

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,16 +6,16 @@ import Layout from "./component/Layout/Layout";
 import Properties from "./pages/Properties/Properties";
 function App() {
   return (
-    <BrowserRouter>
-      <Suspense fallback={<div>loading....</div>}>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Website />} />
-            <Route path="/properties" element={<Properties />} />
-          </Route>
-        </Routes>
-      </Suspense>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Suspense fallback={<div>loading....</div>}>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Website />} />
+              <Route path="/properties" element={<Properties />} />
+            </Route>
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
   );
 }
 
--- client/src/pages/Properties/Properties.jsx
+++ client/src/pages/Properties/Properties.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import "./properties.css";
+import SearchBar from '../../component/SerachBar/SearchBar';
 const Properties = () => {
   return (
-    <div className='wrapper'>Properties</div>
+    <div className='wrapper'>
+      <div className='flexColCenter paddings innerWidth properties-container'>
+        <SearchBar />
+      </div>
+    </div>
   )
 }
 
