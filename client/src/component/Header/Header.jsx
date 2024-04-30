c25af2a93764b54a1d70a23f535131a553d180d3
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-04-30 19:57:15 +0530

    Added the auth0 using auth0 webiste

--- client/src/component/Header/Header.jsx
+++ client/src/component/Header/Header.jsx
@@ -3,8 +3,10 @@ import "./Header.css";
 import { BiMenuAltRight } from "react-icons/bi";
 import OutsideClickHandler from "react-outside-click-handler";
 import { Link, NavLink } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const { loginWithRedirect, isAuthenticated, user, logout } = useAuth0();
 
   const handleMenuBtnAction = () => {
     setMenuOpen((prev) => !prev);
@@ -23,12 +25,18 @@ const Header = () => {
         </Link>
         <OutsideClickHandler onOutsideClick={() => setMenuOpen(false)}>
           <div className="flexCenter h-menu" style={getMenuStyles(menuOpen)}>
-          <NavLink to="/properties">Properties</NavLink>
+            <NavLink to="/properties">Properties</NavLink>
             <a href="sushantsrk2001@gmail.com">Contacts</a>
 
-            <button className="button">
-              Login  
-            </button>
+            {
+              !isAuthenticated?
+              <button className="button" onClick={loginWithRedirect}>
+                Login
+              </button> : 
+              <div>
+                User Profile
+              </div>
+            }
           </div>
         </OutsideClickHandler>
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const domain = import.meta.env.VITE_DOMAIN;
+const clientId = import.meta.env.VITE_CLIENTID;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirectUri:"http://localhost:5173/"
+      }}
+      audience="http://localhost:6969"
+      scope="openid profile email"
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
